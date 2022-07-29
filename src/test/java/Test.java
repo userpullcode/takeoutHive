35e2e5dc5eec8c0373dbcfba148c0dbf30ef2bcd
xiaocao <1685822377@qq.com>
2022-07-29 20:51:52 +0800

    ideaPush second commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,6 +4,5 @@ public class Test {
     @org.junit.jupiter.api.Test
     public void test(){
         System.out.println(UUID.randomUUID().toString().replaceAll("-",""));
-        System.out.println("新加的");
     }
 }
