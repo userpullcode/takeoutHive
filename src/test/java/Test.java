07db917c5f13bb84eb6aece802667feec0508c49
userpullcode <110145907+userpullcode@users.noreply.github.com>
2022-07-29 20:46:57 +0800

    first commit

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -4,5 +4,6 @@ public class Test {
     @org.junit.jupiter.api.Test
     public void test(){
         System.out.println(UUID.randomUUID().toString().replaceAll("-",""));
+        System.out.println("新加的");
     }
 }
